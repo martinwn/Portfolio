4ef4d91070b647f35010a6b4b9ca2a930628aa35
unknown <martinwn@vcu.edu>
2018-09-17 05:08:57 -0400

    added h1 to header, reformatted media-queries

--- js/index.js
+++ js/index.js
@@ -1,39 +1,71 @@
 $(document).ready(function() {
+  function changeCss1() {
+    const rgba1 = "rgba(0,0,0,0)";
+    const rgba2 = "rgba(0,0,0,0.2)";
+    const element1 = document.querySelector("nav");
+    const element2 = document.querySelector("#projects");
+    const position = element2.getBoundingClientRect();
+    if (position.y < 100 && position.y > -position.height + 50) {
+      element1.style.background = rgba2;
+    } else {
+      element1.style.background = rgba1;
+    }
+    //   ? (element1.style.background = rgba2)
+    //   : (element1.style.background = rgba1);
+  }
 
-    $(document).on("click", "#about-link", function() {
+  window.addEventListener("scroll", changeCss1, false);
 
-        event.preventDefault();
-    
-        $([document.documentElement, document.body]).animate({
-            scrollTop: $("#about").offset().top
-        }, 800);
-    });
+  $(document).on("click", "#will", function() {
+    $([document.documentElement, document.body]).animate(
+      {
+        scrollTop: $(".container").offset().top
+      },
+      800
+    );
+  });
 
-    $(document).on("click", "#projects-link", function() {
+  $(document).on("click", "#about-link", function() {
+    event.preventDefault();
 
-        event.preventDefault();
-    
-        $([document.documentElement, document.body]).animate({
-            scrollTop: $("#projects").offset().top
-        }, 800);
-    });
+    $([document.documentElement, document.body]).animate(
+      {
+        scrollTop: $("#about").offset().top
+      },
+      800
+    );
+  });
 
-    $(document).on("click", "#contact-link", function() {
+  $(document).on("click", "#projects-link", function() {
+    event.preventDefault();
 
-        event.preventDefault();
-    
-        $([document.documentElement, document.body]).animate({
-            scrollTop: $("#contact").offset().top
-        }, 800);
-    });
+    $([document.documentElement, document.body]).animate(
+      {
+        scrollTop: $("#projects").offset().top
+      },
+      800
+    );
+  });
 
-    $(document).on("click", "#footer-link", function() {
+  $(document).on("click", "#contact-link", function() {
+    event.preventDefault();
 
-        event.preventDefault();
-    
-        $([document.documentElement, document.body]).animate({
-            scrollTop: $(".container").offset().top
-        }, 1400);
-    });
-})
+    $([document.documentElement, document.body]).animate(
+      {
+        scrollTop: $("#contact").offset().top
+      },
+      800
+    );
+  });
 
+  $(document).on("click", "#footer-link", function() {
+    event.preventDefault();
+
+    $([document.documentElement, document.body]).animate(
+      {
+        scrollTop: $(".container").offset().top
+      },
+      1400
+    );
+  });
+});
